3683682cacec78b121630c8a2838c234de7ddff2
Chris Cuber <k.cuber@braunmann.com>
2021-12-21 14:23:11 +0100

    Resource guru - Flatten array, not arrays input - Green tests

--- js/resource_guru/task1.js
+++ js/resource_guru/task1.js
@@ -1,7 +1,7 @@
 /**
  *  Flattening an multiple nested array function
  */
-const flatArray = array => array.reduce((arr, item) =>
+const flatArray = array => !Array.isArray(array) ? [] : array.reduce((arr, item) =>
     Array.isArray(item) ? arr.concat(flatArray(item)) : arr.concat([item])
 , []);
 
