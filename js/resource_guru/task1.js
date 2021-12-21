61e8a524512bed9a3b452ec6bfd6b9c74e80874b
Chris Cuber <k.cuber@braunmann.com>
2021-12-21 13:40:12 +0100

    Resource guru - 4th refactor using concat function instead of push

--- js/resource_guru/task1.js
+++ js/resource_guru/task1.js
@@ -1,11 +1,8 @@
 /**
  *  Flattening an multiple nested array function
  */
-const flatArray = array => array.reduce((arr, item) => {
-    // Do recursion and decompose to concat array if item is a type of array, otherwise push to flattened array
-    Array.isArray(item) ? arr.push(...flatArray(item)) : arr.push(item);
-    // Return reduced array
-    return arr;
-}, []);
+const flatArray = array => array.reduce((arr, item) =>
+    Array.isArray(item) ? arr.concat(flatArray(item)) : arr.concat([item])
+, []);
 
 module.exports = flatArray;
