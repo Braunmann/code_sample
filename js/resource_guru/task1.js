5ada00b5a473c6a43834988a5b3eca8096026878
Chris Cuber <k.cuber@braunmann.com>
2021-12-21 13:35:59 +0100

    Resource guru - 3rd refactor to arrow function

--- js/resource_guru/task1.js
+++ js/resource_guru/task1.js
@@ -1,14 +1,11 @@
 /**
  *  Flattening an multiple nested array function
  */
-function flatArray(array) {
-    // Reduce array
-    return array.reduce((arr, item) => {
-        // Do recursion and decompose to concat array if item is a type of array, otherwise push to flattened array
-        Array.isArray(item) ? arr.push(...flatArray(item)) : arr.push(item);
-        // Return reduced array
-        return arr;
-    }, []);
-}
+const flatArray = array => array.reduce((arr, item) => {
+    // Do recursion and decompose to concat array if item is a type of array, otherwise push to flattened array
+    Array.isArray(item) ? arr.push(...flatArray(item)) : arr.push(item);
+    // Return reduced array
+    return arr;
+}, []);
 
 module.exports = flatArray;
