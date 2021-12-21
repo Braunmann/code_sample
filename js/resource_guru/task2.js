8da092bfd48281bc539f37539bfa72dc7cbed812
Chris Cuber <k.cuber@braunmann.com>
2021-12-21 15:53:49 +0100

    Resource guru - 2nd task - 3rd - refactor to ES2015 class and overloading valueOf()

--- js/resource_guru/task2.js
+++ js/resource_guru/task2.js
@@ -1,56 +1,52 @@
 // In TypeScript I would prefer to do the node as Abstract class to be sure that we are using proper composition
-const AbstractNode = (operator, left, right) => {
-    const isNode = n => typeof(n) === 'object' && n.left && n.right
+class AbstractNode {
+    constructor(operator, left, right) {
+        this.operator = operator;
+        this.left = left;
+        this.right = right;
+    }
 
-    const result = function () {
-        // Check both sides if it's node then call recursive to reduce to Number
-        const l = isNode(left) ? left.result() : left,
-              r = isNode(right) ? right.result() : right;
-
-        return operator.operation(l, r);
+    valueOf() {
+        return this.operator.operation(this.left, this.right);
     };
 
-    const toString = function () {
-        return `(${left.toString()} ${operator.textRepresentation} ${right.toString()})`;
+    toString() {
+        return `(${this.left.toString()} ${this.operator.textRepresentation} ${this.right.toString()})`;
     };
 
-    return {
-        operator,
-        left,
-        right,
-        result,
-        toString
-    };
-};
-
-// In Type script will be good to create interface for the operators
-const addOperator = {
-    operation: (l, r) => l + r,
-    textRepresentation: '+'
-}
-const subOperator = {
-    operation: (l, r) => l - r,
-    textRepresentation: '-'
-}
-const divOperator = {
-    operation: (l, r) => l / r,
-    textRepresentation: '÷'
-}
-const mulOperator = {
-    operation: (l, r) => l * r,
-    textRepresentation: 'x'
+    result() {
+        return this.valueOf();
+    }
 }
 
 const NodeFactory = (operationType, left, right) => {
+    // In Type script will be good to create interface for the operators
+    const addOperator = {
+        operation: (l, r) => l + r,
+        textRepresentation: '+'
+    }
+    const subOperator = {
+        operation: (l, r) => l - r,
+        textRepresentation: '-'
+    }
+    const divOperator = {
+        operation: (l, r) => l / r,
+        textRepresentation: '÷'
+    }
+    const mulOperator = {
+        operation: (l, r) => l * r,
+        textRepresentation: 'x'
+    }
+
     switch (operationType) {
         case "+":
-            return AbstractNode(addOperator, left, right);
+            return new AbstractNode(addOperator, left, right);
         case "-":
-            return AbstractNode(subOperator, left, right);
+            return new AbstractNode(subOperator, left, right);
         case "÷":
-            return AbstractNode(divOperator, left, right);
+            return new AbstractNode(divOperator, left, right);
         case "x":
-            return AbstractNode(mulOperator, left, right);
+            return new AbstractNode(mulOperator, left, right);
         default:
             // TODO Throw exception?
     }
--- js/resource_guru/task2.test.js
+++ js/resource_guru/task2.test.js
@@ -5,50 +5,50 @@ describe('(Task 2) Binary Tree', () => {
         const node = NodeFactory("+", 1, 3);
 
         test('Should be presented as (1 + 3)', () => {
-            expect(node.toString()).toStrictEqual("(1 + 3)");
+            expect(node.toString()).toBe("(1 + 3)");
         });
         test('Should equal 4 ', () => {
-            expect(node.result()).toStrictEqual(4);
+            expect(node.result()).toBe(4);
         });
     });
 
     describe('Node sub 3 - 1 = 2 ', () => {
         const node = NodeFactory("-", 3, 1);
         test('Should be presented as (3 - 1)', () => {
-            expect(node.toString()).toStrictEqual("(3 - 1)");
+            expect(node.toString()).toBe("(3 - 1)");
         });
         test('Should equal 2 ', () => {
-            expect(node.result()).toStrictEqual(2);
+            expect(node.result()).toBe(2);
         });
     });
 
     describe('Node div 9 / 3 = 3 ', () => {
         const node = NodeFactory("÷", 9, 3);
         test('Should be presented as (9 ÷ 3)', () => {
-            expect(node.toString()).toStrictEqual("(9 ÷ 3)");
+            expect(node.toString()).toBe("(9 ÷ 3)");
         });
         test('Should equal 3 ', () => {
-            expect(node.result()).toStrictEqual(3);
+            expect(node.result()).toBe(3);
         });
     });
 
     describe('Node mul 4 * 3 = 12', () => {
         const node = NodeFactory("x", 4, 3);
         test('Should be presented as (4 x 3)', () => {
-            expect(node.toString()).toStrictEqual("(4 x 3)");
+            expect(node.toString()).toBe("(4 x 3)");
         });
         test('Should equal 12', () => {
-            expect(node.result()).toStrictEqual(12);
+            expect(node.result()).toBe(12);
         });
     });
 
     describe('Node addition integer to another node 1 + (3 + 4) = 12', () => {
         const tree = NodeFactory("+", 1, NodeFactory("+", 3, 4));
         test('Should be presented as (1 + (3 + 4))', () => {
-            expect(tree.toString()).toStrictEqual("(1 + (3 + 4))");
+            expect(tree.toString()).toBe("(1 + (3 + 4))");
         });
         test('Should equal 8', () => {
-            expect(tree.result()).toStrictEqual(8);
+            expect(tree.result()).toBe(8);
         });
     });
 
@@ -66,10 +66,10 @@ describe('(Task 2) Binary Tree', () => {
         6
     );
 
-    test('First assertion', () => {
+    test('Resource Guru: First assertion', () => {
         expect(tree.toString()).toStrictEqual("((7 + ((3 - 2) x 5)) ÷ 6)");
     });
-    test('Second assertion', () => {
+    test('Resource Guru: Second assertion', () => {
         expect(tree.result()).toStrictEqual(2);
     });
 });
